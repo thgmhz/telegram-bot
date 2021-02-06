3cba97666038385e27d09e68dc930bd0b01ea6aa
Thiago Medeiros <thiagocontaparatestes@gmail.com>
2021-02-06 14:43:36 +0000

    bot: refactor to class syntax

---
+++ src/Bot.js
@@ -0,0 +1,267 @@
+const { Composer } = require('micro-bot')
+const Telegraf = require('telegraf')
+const path = require('path')
+
+const captchaList = require('./captcha')
+
+class Bot {
+  constructor({ token, isDev }) {
+    this.bot = isDev
+      ? new Telegraf(token)
+      : new Composer
+    
+    if (isDev) this.bot.launch()
+
+    this.isDev = isDev
+    this.maxAttempts = 3
+    this.strictCase = false
+    this.usersBlacklist = []
+    this.captchaTimeout = 180000
+  }
+
+  init({ username, maxAttempts, captchaTimeout, strictCase }) {
+    this.botUsername = username
+    this.maxAttempts = maxAttempts || this.maxAttempts
+    this.captchaTimeout = captchaTimeout || this.captchaTimeout
+    this.strictCase = strictCase
+
+    this.bot.on('new_chat_members', this.onNewChatMembers.bind(this))
+    this.bot.on('message', this.onNewMessage.bind(this))
+  }
+
+  getRandomCaptcha() {
+    const randomNumber = Math.floor(Math.random() * captchaList.length) + 0
+
+    return captchaList[randomNumber]
+  }
+
+  setMessages({ welcome, timeout, attemptFail, attemptsOver, captchaSuccess }) {
+    this.welcomeMessage = welcome
+    this.timeoutMessage = timeout
+    this.attemptFailMessage = attemptFail
+    this.attemptsOverMessage = attemptsOver
+    this.captchaSuccessMessage = captchaSuccess
+  }
+
+  replaceAll(str, find, replace) {
+    const escapedFind = find.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, '\\$1')
+    return str.replace(new RegExp(escapedFind, 'g'), replace)
+  }
+
+  parseMessage({ user, message }) {
+    let msg = this.replaceAll(message, '$firstname', user.first_name)
+    msg = this.replaceAll(msg, '$lastname', user.last_name || '')
+    msg = this.replaceAll(msg, '$username', `@${user.username}` || '')
+    msg = this.replaceAll(msg, '$attemptCount', user.attempt)
+
+    return msg
+  }
+
+  addUserToBlacklist(user) {
+    this.usersBlacklist.push(user)
+  }
+
+  getUserFromBlacklist(userId) {
+    return this.usersBlacklist.find(usr => usr.id === userId)
+  }
+
+  removeUserFromBlacklist(userId) {
+    this.usersBlacklist = this.usersBlacklist.filter(usr => usr.id !== userId)
+  }
+
+  updateUserAttempts({ userId, attempt }) {
+    const user = this.getUserFromBlacklist(userId)
+
+    user.attempt = attempt
+
+    this.usersBlacklist = this.usersBlacklist.map(usr => 
+      usr.id === userId
+        ? user 
+        : usr
+    )
+  }
+
+  updateUserMessages({ userId, messagesIds }) {
+    const user = this.getUserFromBlacklist(userId)
+
+    user.messages = [
+      ...user.messages,
+      ...messagesIds,
+    ]
+
+    this.usersBlacklist = this.usersBlacklist.map(usr => 
+      usr.id === userId
+        ? user 
+        : usr
+    )
+  }
+
+  deleteMessages({ context, messages }) {
+    messages.forEach(id => {
+      if (id) context.deleteMessage(id).catch(console.log)
+    })
+  }
+
+  acceptUser({ context, user }) {
+    this.removeUserFromBlacklist(user.id)
+
+    const message = this.parseMessage({
+      user,
+      message: this.captchaSuccessMessage,
+    })
+
+    context.reply(message)
+
+    this.deleteMessages({
+      context,
+      messages: user.messages,
+    })
+  }
+
+  rejectUser({ context, user, message }) {
+    const userInBlacklist = this.getUserFromBlacklist(user.id)
+
+    if (userInBlacklist) {
+      this.removeUserFromBlacklist(user.id)
+      this.deleteMessages({
+        context,
+        messages: userInBlacklist.messages,
+      })
+      context.kickChatMember(user.id)
+      context.reply(message)
+    }
+  }
+
+  async newAttempt({ context, user, messageId }) {
+    user.attempt += 1
+    const canTryAgain = (user.attempt < this.maxAttempts)
+
+    if (!canTryAgain) {
+      const attemptsOver = this.parseMessage({
+        user,
+        message: this.attemptsOverMessage,
+      })
+      
+      this.updateUserMessages({
+        userId: user.id,
+        messagesIds: [
+          messageId,
+        ],
+      })
+
+      return this.rejectUser({
+        context,
+        user,
+        message: attemptsOver,
+      })
+    }
+
+    const attemptFail = this.parseMessage({
+      user,
+      message: this.attemptFailMessage,
+    })
+
+    const failMessage = await context.reply(attemptFail)
+    
+    this.updateUserAttempts({
+      userId: user.id,
+      attempt: user.attempt,
+    })
+
+    this.updateUserMessages({
+      userId: user.id,
+      messagesIds: [
+        messageId,
+        failMessage.message_id,
+      ],
+    })
+  }
+
+  async onNewChatMembers(context) {
+    const { message } = context
+    const { new_chat_participant } = message
+    const { id, first_name, last_name, username } = new_chat_participant
+
+    if (username === this.botUsername) return
+
+    const user = {
+      id, 
+      first_name,
+      last_name,
+      username,
+    }
+
+    const captcha = this.getRandomCaptcha() || captchaList[0]
+
+    const welcomeMessage = this.parseMessage({
+      user,
+      message: this.welcomeMessage
+    })
+
+    const welcome = await context.replyWithPhoto({ 
+      source: path.join(__dirname, `/captcha/images/${captcha.image}`)
+    }, { 
+      caption: welcomeMessage
+    })
+
+    this.addUserToBlacklist({
+      ...user,
+      captcha,
+      attempt: 0,
+      messages: [welcome.message_id]
+    })
+
+    setTimeout(() => {
+      const userInBlacklist = this.getUserFromBlacklist(user.id)
+
+      if (userInBlacklist) {
+        const message = this.parseMessage({
+          user,
+          message: this.timeoutMessage,
+        })
+        
+        this.rejectUser({
+          user, 
+          context, 
+          message,
+        })
+      }
+    }, this.captchaTimeout)
+  }
+
+  onNewMessage(context) {
+    const { message } = context
+    const { message_id, from, text } = message
+
+    const userInBlacklist = this.getUserFromBlacklist(from.id)    
+    
+    if (userInBlacklist) {
+      const { captcha } = userInBlacklist
+      const { code } = captcha
+
+      const userSolvedCaptcha = this.strictCase
+        ? (text === code)
+        : (text.toLowerCase() === code.toLowerCase())
+
+      if(!userSolvedCaptcha) {
+        return this.newAttempt({
+          context,
+          user: userInBlacklist,
+          messageId: message_id,
+        })        
+      }
+
+      this.updateUserMessages({
+        userId: userInBlacklist.id,
+        messagesIds: [message_id],
+      })
+
+      this.acceptUser({
+        context, 
+        user: userInBlacklist, 
+      })      
+    }
+  }
+}
+
+module.exports = Bot
--- src/index.js
+++ src/index.js
@@ -1,161 +1,26 @@
 require('dotenv').config()
 
-const Telegraf = require('telegraf')
-const { Composer } = require('micro-bot')
-const path = require('path')
-const captcha = require('./captcha.js')
+const Bot = require('./Bot.js')
 
-const isDev = process.argv[2] === 'isDev' || false
+const token = process.env.BOT_TOKEN
+const isDev = process.argv[2] === 'isDev'
 
-const bot = isDev
-  ? new Telegraf(process.env.BOT_TOKEN)
-  : new Composer
+const bot = new Bot({ token, isDev })
 
-const App = {
-  usersInCaptcha: [],
+bot.setMessages({
+  welcome: 'Olá $firstname!\n\nATENÇÃO: Para garantir que você não é um robô de spam, envie uma mensagem com as letras e números que aparecem na imagem acima.\n\nVocê tem 3 tentativas.\n\nSe as tentativas não forem feitas dentro de 5 minutos você será removido(a) do grupo automaticamente.\n\n$username',
+  timeout: '🚨 $firstname não digitou o código e foi removido(a).',
+  attemptFail: '$firstname, código inválido! Você tem mais $attemptCount tentativas.',
+  attemptsOver: '🚨 $firstname não digitou o código corretamente e foi removido(a).',
+  captchaSuccess: '👍 Ok, $firstname não é um robô spam.\n\n$firstname, seja bem-vindo(a)!\n\nNão esqueça de ler as regras na mensagem fixada no topo do grupo.'
+})
 
-  init() {
-    isDev && bot.launch()
-
-    bot.on('new_chat_members', this.Events.onMemberEnter.bind(this))
-    bot.on('message', this.Events.onNewMessage.bind(this))
-  },  
-
-  getRandomCaptcha() {
-    const randomNumber = Math.floor(Math.random() * captcha.length) + 0
-
-    return captcha[randomNumber]
-  },
-
-  kickUserInCaptcha({ ctx, id, message }) {
-    const user = this.usersInCaptcha.find(user => user.id === id)
-
-    if (user) {
-      this.removeUserFromCaptchaList(user.id)
-      ctx.kickChatMember(id)
-      ctx.reply(message)
-    }
-  },
-
-  deleteMessages(ctx, messagesId) {
-    messagesId.forEach(id => {
-      if (id) ctx.deleteMessage(id).catch(console.log)
-    })
-  },
-
-  removeUserFromCaptchaList(id) {
-    this.usersInCaptcha = this.usersInCaptcha.filter(user => user.id !== id)
-  },
-
-  Events: {
-    async onNewMessage(ctx) {
-      console.log('#### on new message')
-
-      const { message } = ctx
-      const { from, text } = message
-
-      console.log('#### users in captcha', this.usersInCaptcha)
-      
-      // check if user is in captcha list
-      const user = this.usersInCaptcha.find(user => user.id === from.id) || null    
-
-      if (user) {
-        if (text.toLowerCase() == user.captcha.code.toLowerCase()) {
-          this.removeUserFromCaptchaList(user.id)
-          
-          await ctx.reply(`👍 Ok, ${user.userString} não é um robô.`)
-          await ctx.reply(`${user.userString}, seja bem-vindo(a)!\n\nNão esqueça de ler as regras na mensagem fixada no topo do grupo.`)
-          
-          if (message && message.message_id) {
-            user.messagesToDelete.push(message.message_id)
-          }
-
-          this.deleteMessages(ctx, user.messagesToDelete)
-        } else {
-          user.attempt -= 1
-
-          if (user.attempt <= 0){
-            this.kickUserInCaptcha({
-              ctx, 
-              id: user.id, 
-              message: `🚨 ${user.userString} não digitou o código corretamente e foi removido(a).`
-            })
-
-            if (message && message.message_id) {
-              user.messagesToDelete.push(message.message_id)
-            }
-    
-            this.deleteMessages(ctx, user.messagesToDelete)
-
-          } else {
-            const msg_invalid = await ctx.reply(`${user.userString}, código inválido, você tem mais ${user.attempt} tentativas. Maiúsculas e minúsculas fazem diferença.`)
-            user.messagesToDelete.push(msg_invalid.message_id)            
-            user.messagesToDelete.push(message.message_id)
-          }
-
-          // update user attempt in captcha list
-          this.usersInCaptcha = this.usersInCaptcha.map(usr => {
-            return usr.id === user.id
-              ? { ...usr, attempt: user.attempt, messagesToDelete: user.messagesToDelete }
-              : usr
-          })
-        }                
-      }
-    },
-    
-    async onMemberEnter(ctx) {
-      console.log('#### onMemberEnter')
-
-      const { message } = ctx
-      const { new_chat_participant } = message
-      const { id, first_name, last_name, username } = new_chat_participant
-
-      if (username === 'TugaBot_bot') return
-
-      let userString = `${first_name}`
-      if (last_name) userString += ` ${last_name}`
-      if (username) userString += ` (@${username})`
-      
-      // await ctx.reply(`${userString} entrou do grupo...`)
-
-      const captcha = this.getRandomCaptcha() || captcha[0]
-
-      const msg_welcome = await ctx.replyWithPhoto({ 
-        source: path.join(__dirname, `/images/${captcha.image}`)
-      }, { 
-        caption: `Olá ${userString}!\n\nATENÇÃO: Para garantir que você não é um robô de spam, envie uma mensagem com as letras e números que aparecem na imagem acima.\n\nVocê tem 3 tentativas.\n\nMaiúsculas e minúsculas fazem diferença.\n\nSe as tentativas não forem feitas dentro de 3 minutos você será removido(a) do grupo automaticamente.`
-      })
-
-      console.log('##### msg_welcome', msg_welcome)
-      console.log('##### msg_welcome.message_id', msg_welcome.message_id)
-      
-      const messagesToDelete = [msg_welcome.message_id] 
-
-      console.log('######### captcha', captcha)
-
-      // add user to captcha list
-      this.usersInCaptcha.push({ 
-        id, 
-        userString,
-        captcha,
-        attempt: 3,
-        messagesToDelete,
-      })  
-
-      setTimeout(() => {
-        this.deleteMessages(ctx, messagesToDelete)
-
-        this.kickUserInCaptcha({
-          ctx, 
-          id, 
-          message: `🚨 ${userString} não digitou o código e foi removido(a).`
-        })
-      }, 180000)          
-    }
-  }
-}
-
-App.init()
+bot.init({
+  username: 'TugaBot_bot',
+  captchaTimeout: 60000 * 5,
+  maxAttempts: 3,
+  strictCase: false,
+})
 
 module.exports = bot
 
